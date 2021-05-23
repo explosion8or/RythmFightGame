96241b2a499cac32d86c9672fa3d2b374d660d00
explosion8or <45521450+explosion8or@users.noreply.github.com>
2021-05-23 13:58:31 -0400

    Added test stuff
    use main for functioning thing

---
+++ RythmNew/Assets/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct BeatHit {
+    public float BeatNumber;
+    public SwipeDirection Direction;
+}
+
+public class Test : MonoBehaviour
+{
+    void Start(){
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
