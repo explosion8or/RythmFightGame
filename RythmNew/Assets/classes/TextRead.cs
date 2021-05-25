732aab704e8d1db62056581a5b7364d0632a6b29
explosion8or <45521450+explosion8or@users.noreply.github.com>
2021-05-25 14:19:10 -0400

    Update TextRead.cs

--- RythmNew/Assets/classes/TextRead.cs
+++ RythmNew/Assets/classes/TextRead.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public struct Hit {
     public float BeatStart;
@@ -12,28 +13,27 @@ public static class TextRead
 {
     public static Hit[] IntakeHits()
     {
-
-        int numberOfHits = 15;
+        //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+        int numberOfHits = 96;
         Hit[] HitList = new Hit[numberOfHits];
         
         //hit with dummy values
 
         for (int i = 0; i < numberOfHits; i++) {
             Hit newHit;
-            newHit.BeatStart = 4+(i*4);
-            newHit.BeatEnd = 7+(i*4);
+            newHit.BeatStart = 13f+(i*4);
+            newHit.BeatEnd = 16+(i*4);
 
             if(i<4){
                 newHit.Direction = (SwipeDirection) i;
             }else{
-                newHit.Direction = 0;
+                newHit.Direction = (SwipeDirection) Random.Range(0, 4);
             }
             
 
             HitList[i] = newHit;
         }
-        return HitList;
-
-       
+        return HitList;  
     }
+
 }
