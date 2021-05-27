4d1eba99bf972a53518585300a4beb3b946450ec
explosion8or <45521450+explosion8or@users.noreply.github.com>
2021-05-27 08:54:20 -0400

    Revert "Final"
    This reverts commit 023415add08fec20b1697eb8e22761c73e06a179.

--- RythmNew/Assets/classes/TextRead.cs
+++ RythmNew/Assets/classes/TextRead.cs
@@ -11,43 +11,29 @@ public struct Hit {
 
 public static class TextRead 
 {
-
-    public static int numberOfHits = 68;
-    public static Hit[] HitList = new Hit[numberOfHits];  
     public static Hit[] IntakeHits()
     {
         //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
-        ;
-        
+        int numberOfHits = 88;
+        Hit[] HitList = new Hit[numberOfHits];
         
         //hit with dummy values
 
-        yeetu(3, 12.5f, 16, 4, 0);
-        yeetu(1, 28.5f, 30, 2, 4);
-        yeetu(3, 32.5f, 36, 4, 6);
-        yeetu(1, 44.5f, 46, 2, 10);
-        yeetu(12, 51.5f, 53, 2, 12);
-        yeetu(9, 77.5f, 80, 4, 26);
-        yeetu(32, 112.5f, 114, 2, 35);
-        
-
-
-        return HitList;  
-    }
-
-    public static void yeetu(int count, float SoS, float EoS, int timePer, int hitNumba){
-        for (int i = 0; i <= count; i++) {
+        for (int i = 0; i < numberOfHits; i++) {
             Hit newHit;
-            newHit.BeatStart = SoS+(i*timePer);
-            newHit.BeatEnd = EoS+(i*timePer);
+            newHit.BeatStart = 13f+(i*4);
+            newHit.BeatEnd = 16+(i*4);
 
             if(i<4){
                 newHit.Direction = (SwipeDirection) i;
             }else{
                 newHit.Direction = (SwipeDirection) Random.Range(0, 4);
             }
-            HitList[hitNumba+i] = newHit;
+            
+        
+            HitList[i] = newHit;
         }
-      
+        return HitList;  
     }
+
 }
